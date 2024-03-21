b4287ad7308360e8885ab997e4dc828d15c8d6cb
Oliver Peter Pinter <125395118+PinterOliver@users.noreply.github.com>
2024-03-21 09:45:55 +0100

    generate Spring Boot backend

---
+++ backend/src/test/java/com/codecool/association/BackendApplicationTests.java
@@ -0,0 +1,11 @@
+package com.codecool.association;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+  @Test
+  void contextLoads() {
+  }
+}
